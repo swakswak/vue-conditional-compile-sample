9466025355953ac2c37d4c50ea41c9f4a7833c2c
swakswak <hschoi0702@gmail.com>
2022-09-26 00:16:28 +0900

    가독성 향상을 위한 줄바꿈

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
+
   <h1 style="color: blue">Public</h1>
+
   /* IFTRUE_isPrivate */
   <h1 style="color: red">Private</h1>
   /* FITRUE_isPrivate */
+
   /* IFDEBUG */
   <h1 style="color: green">DEBUG</h1>
   /* FIDEBUG */
+
 </template>
 
 <script>
